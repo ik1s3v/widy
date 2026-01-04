0c8c3159d801c8c754dc93e4879e984c3fa59e38
ik1s3v <ik1s3v@gmail.com>
2026-01-04 02:41:05 +0200

    fix:build env

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,19 +1,3 @@
 fn main() {
-    dotenvy::dotenv().ok();
-
-    let api_id: i32 = std::env::var("API_ID")
-        .expect("API_ID must be set")
-        .parse()
-        .expect("API_ID must be a valid i32");
-
-    let api_hash: String = std::env::var("API_HASH").expect("API_HASH must be set");
-
-    let client_id: String =
-        std::env::var("TWITCH_CLIENT_ID").expect("TWITCH_CLIENT_ID must be set");
-    println!("cargo:rustc-env=API_ID={}", api_id);
-
-    println!("cargo:rustc-env=API_HASH={}", api_hash);
-
-    println!("cargo:rustc-env=TWITCH_CLIENT_ID={}", client_id);
     tauri_build::build()
 }
